e45110102d8da3fc9c78d4951b546727ed2f1ccf
vivekthiru <vivek83_t@yahoo.com>
2016-03-07 23:24:48 +0800

    Added files via upload

--- static/js/frameworkctrl.js
+++ static/js/frameworkctrl.js
@@ -1,33 +1,74 @@
-var frameworkApp = angular.module('FrameworkModule', []);
+var frameworkApp = angular.module('FrameworkModule', ['ngAnimate']);
 
 frameworkApp.controller('FrameworkController',function ($scope) {
 	
 	$scope.tabs=[{"url":"angulardemo","display":"Angular Demo"},{"url":"stockdemo","display":"Stock Demo"},{"url":"underconstruction","display":"demo3"},{"url":"underconstruction","display":"demo4"}];
 	//alert(tabs[1].url);
 	$scope.mainpage="link/?url="+$scope.tabs[0].url;
-	$scope.headerpage="header";
-	$scope.footerpage="footer";
+	//$scope.mainpage="link/?url=underconstruction";
+	$scope.headerpage="link/?url=header";
+	$scope.footerpage="link/?url=footer";
 	//alert($scope.mainpage);
 	
 	
 	$scope.linkclick =  function(tab){
-		//alert("inside click -- " + url);
+		alert("inside click -- " + tab.url);
 		//alert($scope.mainpage);
 		$scope.mainpage="link/?url="+tab.url;
 		$scope.linkstyle=tab.display;
+		//$scope.apply;
+		//alert($scope.mainpage);
 	}
 	
-
-	
 });
 
-frameworkApp.controller('StockController', function ($scope,$http) {
+frameworkApp.controller('StockController', function ($parse,$scope,$http) {
 	
 	 var url = "/SpringLearn/starter";
-	//alert(url);
+	 $scope.model = {};
+	 
+	 //alert(url);
 	 $http.get(url).success( function(response) {
-        //alert(response.id);
-		 $scope.students = response;
-});
+		 $scope.contents = response.content;
+		 $scope.headers = response.header;
+	 });
+	 
+	 $scope.addRow= function(){
+		var addcontent='{';
+			angular.forEach($scope.headers,function(header){
+			addcontent = addcontent+"'"+header.variablename +"':"+'model.'+header.variablename+",";
+		});
+		addcontent = addcontent.slice(0, -1) +"}";
+		var expr =$parse(addcontent); 
+		 $scope.contents.push(expr(this));
+		 
+		 //Clear text box
+		 angular.forEach($scope.headers,function(header){
+				$scope.$eval('model.'+header.variablename+'=""'); 
+		});
+	 };
+	 
+	 $scope.deleteRow= function(index){
+			 $scope.contents.splice(index,1);
+		 };
 });
 
+
+
+
+/*frameworkApp.config(function($httpProvider){
+		$httpProvider.interceptors.push(function($q) {
+			 return {
+			     'request': function(config) {
+			        // intercepts the request
+			     },
+			     'response': function(response) {
+			       // intercepts the response. you can examine things like status codes
+			     },
+			     'responseError': function(response) {
+			       // intercepts the response when the response was an error
+			     }
+			  }
+  });
+});
+*/
